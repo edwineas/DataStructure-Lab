9205ac601a26557b9d8db3a5ac1d865615ef2e3a
edwineas <96735074+edwineas@users.noreply.github.com>
2022-10-17 15:07:59 +0530

    Update insertionsort.c

--- insertionsort.c
+++ insertionsort.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
-void swap(int a[],int i,int j)
+void insertionsort(int a[],int n)
 {
-	int t;
-	t = a[i];
-	a[i]=a[j];
-	a[j]= t;
-}
-void bubblesort(int a[],int n)
-{
-	int i,j;
-	for(i=0;i<n;i++)
-		for(j=0;j<n-i;j++)
-			if(a[j]>a[j+1])
-				swap(a,j,j+1);
+	int i,j,x;
+	for(i=2;i<=n;i++)
+	{
+		x=a[i];
+		for(j=i-1;j>=1;j--)
+			if(x<a[j])
+				a[j+1]=a[j];
+			else
+				break;
+		a[j+1]=x;
+	}
+	
 }
 void main()
 {
@@ -20,13 +20,13 @@ void main()
 	printf("Enter the number of elements of array to be sorted\t");
 	scanf("%d",&n);
 	printf("Enter the elements of the array\n");
-	for(i=0;i<n;i++)
+	for(i=1;i<=n;i++)
 	{
 		scanf("%d",&a[i]);		
 	}
-	bubblesort(a,n);
+	insertionsort(a,n);
 	printf("Sorted array is : \n");
-	for(i=0;i<n;i++)
+	for(i=1;i<=n;i++)
 	{
 		printf("%d",a[i]);
 		printf(" ");		
